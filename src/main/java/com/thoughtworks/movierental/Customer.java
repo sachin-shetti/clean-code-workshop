124d73e63b78ddf7d91a64e930a24da4bf24c848
Sachin A <sachin.a@ninestars.in>
2019-11-28 16:26:11 +0530

    seperated statement functions to do one thing

--- src/main/java/com/thoughtworks/movierental/Customer.java
+++ src/main/java/com/thoughtworks/movierental/Customer.java
@@ -21,37 +21,13 @@ public class Customer {
 
     public String statement() {
         double totalAmount = 0;
-        int frequentRenterPoints = 0;
         String result = "Rental Record for " + getName() + "\n";
-        for (Rental each : rentals) {
-            double thisAmount = 0;
-            //determine amounts for each line
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    break;
-            }
-            // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-                    &&
-                    each.getDaysRented() > 1) frequentRenterPoints++;
+        int frequentRenterPoints = frequentRenterPoints();
 
-            //show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" +
-                    thisAmount + "\n";
-            totalAmount += thisAmount;
+        for (Rental rental : rentals) {
+            result += "\t" + rental.getMovie().getTitle() + "\t" +
+                    rental.amount() + "\n";
+            totalAmount += rental.amount();
         }
 
         //add footer lines result
@@ -60,5 +36,17 @@ public class Customer {
                 + " frequent renter points";
         return result;
     }
+
+    private int frequentRenterPoints() {
+        int frequentRenterPoints=0;
+        for (Rental rental : rentals) {
+            frequentRenterPoints++;
+            if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE)
+                    &&
+                    rental.getDaysRented() > 1) frequentRenterPoints++;
+        }
+        return frequentRenterPoints;
+    }
+
 }
 
