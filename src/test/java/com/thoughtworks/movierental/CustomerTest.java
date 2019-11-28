e87a5451a3697a5784ff7a34689962a5983613f4
Sachin A <sachin.a@ninestars.in>
2019-11-28 21:43:39 +0530

    final commit

--- src/test/java/com/thoughtworks/movierental/CustomerTest.java
+++ src/test/java/com/thoughtworks/movierental/CustomerTest.java
@@ -36,4 +36,28 @@ public class CustomerTest {
                 "<b>You earned 8 frequent renter points</b>",customer.htmlStatement());
     }
 
+    @Test
+    public void htmlAndTextStatement() {
+        Customer customer = new Customer("foo");
+        customer.addRental(new Rental(new Movie("movie 1",Movie.CHILDRENS),1));
+        customer.addRental(new Rental(new Movie("movie 2",Movie.REGULAR),2));
+        customer.addRental(new Rental(new Movie("movie 3",Movie.NEW_RELEASE),3));
+        customer.addRental(new Rental(new Movie("movie 4",Movie.SCIFI),4));
+        Assert.assertEquals("<h3>Rental Record for foo</h3>\n" +
+                "<p>\tmovie 1\t1.5</p>\n" +
+                "<p>\tmovie 2\t2.0</p>\n" +
+                "<p>\tmovie 3\t9.0</p>\n" +
+                "<p>\tmovie 4\t16.0</p>\n" +
+                "<b>Amount owed is 28.5</b>\n" +
+                "<b>You earned 8 frequent renter points</b>",customer.htmlStatement());
+
+        Assert.assertEquals("Rental Record for foo\n" +
+                "\tmovie 1\t1.5\n" +
+                "\tmovie 2\t2.0\n" +
+                "\tmovie 3\t9.0\n" +
+                "\tmovie 4\t16.0\n" +
+                "Amount owed is 28.5\n" +
+                "You earned 8 frequent renter points",customer.statement());
+    }
+
 }
